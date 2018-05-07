b62fc1caa307a05f93df76317f23f16c94aca191
James Hunt <james@niftylogic.com>
2018-05-06 20:14:37 -0400

    Start on the LISP lexer/parser

--- rook.c
+++ rook.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <ctype.h>
 
 struct value;
 
@@ -85,6 +86,54 @@ new_symbol(const char *name)
 	return v;
 }
 
+static void
+free_value(struct value *v)
+{
+	switch (v->type) {
+	case STRING:
+		free(v->string.data);
+		break;
+
+	case SYMBOL:
+		free(v->symbol->name);
+		free(v->symbol);
+		break;
+
+	case CONS:
+		/* noop */
+		break;
+	}
+
+	free(v);
+}
+
+static void
+fprintv(FILE *io, int in, struct value *v)
+{
+	char *pre;
+
+	pre = mems(in + 1, char);
+	memset(pre, ' ', in);
+
+	switch (v->type) {
+	case CONS:
+		fprintf(io, "%s(", pre);
+		fprintv(io, 0, v->cons.car);
+		fprintf(io, "\n");
+		fprintv(io, in+1, v->cons.cdr);
+		fprintf(io, ")\n");
+		break;
+
+	case STRING:
+		fprintf(io, "%s\"%s\"", pre, v->string.data);
+		break;
+
+	case SYMBOL:
+		fprintf(io, "%s:%s", pre, v->symbol->name);
+		break;
+	}
+}
+
 static char *
 slurp(const char *file)
 {
@@ -101,7 +150,7 @@ slurp(const char *file)
 		return NULL;
 	}
 
-	p = src = mems(n+1, char);
+	p = src = mems((size_t)n+1, char);
 	while ((nread = read(fd, p, n)) > 0) {
 		p += nread;
 		n -= nread;
@@ -116,10 +165,183 @@ slurp(const char *file)
 	return src;
 }
 
+enum ttype {
+	T_OPENP,
+	T_CLOSEP,
+	T_IDENT,
+	T_STRING,
+};
+
+struct pos {
+	const char *src;
+
+	int offset;
+	int line;
+	int column;
+};
+
+/* copy_pos(&dst, &src)
+
+   set all members of the dst pos structure
+   to the values of corresponding members of
+   the src pos structure.
+ */
+#define copy_pos(dst,src) memcpy((dst), (src), sizeof(struct pos))
+
+/* init_pos(&pos, "src")
+
+   initialize a pos struct to point to the
+   beginning of the raw source code string
+   given as the second argument.
+ */
+static inline void
+init_pos(struct pos *pos, const char *src)
+{
+	pos->offset = 0;
+	pos->line = pos->column = 1;
+	pos->src = src;
+}
+
+struct token {
+	enum ttype  type;
+
+	struct pos  pos;
+	int         len;
+
+	char       *data;
+};
+
+struct lexer {
+	const char *src;
+
+	struct pos prev;
+	struct pos head;
+};
+
+static struct token *
+new_token(struct lexer *l, enum ttype type, char *data)
+{
+	struct token *token;
+
+	token = make(struct token);
+	token->type = type;
+	copy_pos(&token->pos, &l->prev);
+	token->len = l->head.offset - l->prev.offset;
+	token->data = data;
+	return token;
+}
+
+static void
+free_token(struct token *token)
+{
+	free(token->data);
+	free(token);
+}
+
+static char *
+lexeme(struct lexer *l)
+{
+	char *s;
+	size_t len;
+
+	len = l->head.offset - l->prev.offset;
+	s = mem(len + 1);
+	memcpy(s, l->src + l->prev.offset, len);
+	return s;
+}
+
+/* peek(&lexer) -> c
+
+   return the character under the cursor,
+   without changing the position.
+ */
+static inline char
+peek(struct lexer *l)
+{
+	return l->src[l->head.offset];
+}
+
+/* next(&lexer)
+
+   advance the cursor one position, updating
+   line / column markers as necessary.
+
+   returns the character that was at offset,
+   before any adjustments took place.
+ */
+static inline char
+next(struct lexer *l)
+{
+	char c = peek(l);
+
+	l->head.offset++;
+	l->head.column++;
+	if (l->src[l->head.offset] == '\n') {
+		l->head.column = 1;
+		l->head.line++;
+	}
+
+	return c;
+}
+
+/* resync(&lexer)
+
+   put the head cursor into the prev cursor,
+   overwriting what was there previously.
+ */
+static inline void
+resync(struct lexer *l)
+{
+	copy_pos(&l->prev, &l->head);
+}
+
+static struct token *
+lex(struct lexer *l)
+{
+	char c;
+
+	/* eat whitespace */
+	for (c = next(l); c && isspace(c); c = next(l));
+	resync(l);
+
+	switch (c) {
+	case '\0': return NULL;
+
+	case '(': return new_token(l, T_OPENP, NULL);
+	case ')': return new_token(l, T_CLOSEP, NULL);
+	//case '"':
+	//case ':':
+	}
+	if (c && isalpha(c)) {
+		for (c = next(l); c && !isspace(c); c = next(l));
+		return new_token(l, T_IDENT, lexeme(l));
+	}
+	return NULL;
+}
+
+static struct value *
+parse(const char *file, const char *src)
+{
+	int i;
+	struct lexer l;
+	struct token *token;
+
+	init_pos(&l.head, src);
+	init_pos(&l.prev, src);
+
+	while ((token = lex(&l)) != NULL) {
+		fprintf(stderr, "token %d, %p %s\n", token->type, token->data, token->data);
+		free_token(token);
+	}
+
+	return NULL;
+}
+
 int
 main(int argc, char **argv)
 {
 	char *src;
+	struct value *ast;
 
 	if (argc != 2) {
 		fprintf(stderr, "USAGE: %s source.rk\n", argv[0]);
@@ -132,7 +354,14 @@ main(int argc, char **argv)
 		exit(1);
 	}
 
-	fprintf(stderr, "========\n%s========\n", src);
+	ast = parse(argv[1], src);
+	if (!ast) {
+		fprintf(stderr, "%s: parsing failed.\n", argv[1]);
+		exit(1);
+	}
+
+	fprintv(stdout, 0, ast);
+	free_value(ast);
 	free(src);
 	return 0;
 }
